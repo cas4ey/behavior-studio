13a7d84b3a65871b70117f5905499b5b5dd8538d
Victor Zarubkin <v.s.zarubkin@gmail.com>
2015-08-09 20:34:52 +0300

    fixed the mistake with [] instead of ()

--- source/treeview/diagram.py
+++ source/treeview/diagram.py
@@ -1228,7 +1228,7 @@ class TreeGraphicsScene(QGraphicsScene):
         for i in range(2):
             for classname in self.project.alphabet.getClasses(True):
                 fin = False
-                cls = self.project.alphabet.getClass[classname]
+                cls = self.project.alphabet.getClass(classname)
                 for tpname in cls.getLinkTypes():
                     t = cls.types[tpname]
                     if i > 0 or not t.isCopyLink():
